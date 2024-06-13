0224c905bec9f5067fd2e340cced46921e034fb1
rifanijuni <rifanijuniarti29@gmail.com>
2024-06-13 15:04:31 +0700

    membuat api

--- routes/api11.php
+++ routes/api11.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KategoriController;
 
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
